0c3fa9771ff16ce538e759572a2b3e2f606f1c79
Sporny <janheidolph@yahoo.de>
2022-01-28 15:31:36 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,40 @@
+Config = {}
+
+Config.Stables = {
+    Valentine = {
+        Pos = {x=-367.73, y=787.72, z=116.26},
+		Name = 'Stable',
+        Heading = -30.65,
+		SpawnPoint = {
+			Pos = {x=-372.43, y=791.79, z=116.13},
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 182.3
+        }
+    }
+}
+
+Config.Horses = {
+	{
+		name = "Category 1",
+		["A_C_HORSEMULE_01"] = {"Mula", 18, 18},
+		["A_C_Horse_KentuckySaddle_Grey"] = {"Kentucky Saddle (Cinza)", 50, 50},
+		["A_C_Horse_Morgan_Palomino"] = {"Morgan", 60, 60},
+		["A_C_Horse_Thoroughbred_DappleGrey"] = {"Thoroughbred", 130, 130},
+		["A_C_Horse_AmericanStandardbred_Buckskin"] = {"Standardbred Americano", 130, 130},
+		["A_C_Horse_AmericanPaint_Overo"] = {"American Paint (Cor-de-pinto)", 130, 130},
+		["A_C_Horse_Andalusian_DarkBay"] = {"Andalusian (Preto Cintilante)", 140, 140},
+		["A_C_Horse_Andalusian_Perlino"] = {"Andalusian (Celeste)", 140, 140},		
+		["A_C_Horse_Nokota_ReverseDappleRoan"] = {"Nokota, o ágil.", 450, 450},
+
+	},
+	{
+		name = "Category 2",
+		["A_C_Horse_Belgian_MealyChestnut"] = {"Belga (Loiro)", 120, 120},
+		["A_C_Horse_Belgian_BlondChestnut"] = {"Belga (Castanho-Claro)", 120, 120},
+		["A_C_Horse_Shire_LightGrey"] = {"Shire (Branco Acizentado)", 130, 130},
+		["A_C_Horse_AmericanPaint_Tobiano"] = {"American Paint (Tobiano)", 140, 140},
+		["A_C_Horse_SuffolkPunch_RedChestnut"] = {"Suffolk Punch (Castanho Avermelhado)", 150, 150},
+		["A_C_Horse_Appaloosa_Blanket"] = {"Appaloosa", 200, 200},
+		["A_C_Horse_Turkoman_Gold"] = {"Turkoman (Dourado Fulgente)", 950, 950},
+	}
+}
